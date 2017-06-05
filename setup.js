889f9a4d513daf0819fc278b17a673d18a545a29
AndrewSenewe <sindrew.christ@gmail.com>
2017-06-05 11:48:51 +0700

    student done

--- setup.js
+++ setup.js
@@ -1,3 +1,44 @@
 "use strict"
 
 //write your code here
+"use strict"
+
+//write your code here
+const repl = require('repl');
+const sqlite3 = require('sqlite3').verbose();
+
+let file = 'student.db'
+let db = new sqlite3.Database(file);
+
+let replServer = repl.start({
+  prompt: '>> ',
+  input: process.stdin,
+  output: process.stdout
+});
+
+var create_query = "CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(100), last_name VARCHAR(100), gender VARCHAR(25), birthday DATE, email VARCHAR(100), phone VARCHAR)";
+var insert_query = "INSERT INTO student (first_name, last_name, gender, birthday, email, phone) VALUES ('Rubi', 'Henjaya', 'Male', '1986-11-20', 'rsanjaya@gmail.com', '+6281323455432'), ('Riza', 'Fahmi', 'Male', '1983-12-31', 'rfahmi@gmail.com', '+6281345543210');";
+
+let createTable = () => {
+  db.serialize(function() {
+    db.run(create_query, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Table created!');
+      }
+    });
+  });
+}
+
+let insertData = () => {
+  db.serialize(function() {
+    db.run(insert_query, (err) => {
+      if (!err) console.log('Data inserted!');
+      else console.log(err);
+    })
+  })
+}
+
+replServer.context.make = createTable;
+replServer.context.insert = insertData;
--- student_roster.js
+++ student_roster.js
@@ -1,6 +1,146 @@
 "use strict"
 
 const repl = require('repl');
-const sqlite = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
 
+let file = 'student.db'
+let db = new sqlite3.Database(file);
+
+let replServer = repl.start({
+  prompt: '>> ',
+  input: process.stdin,
+  output: process.stdout
+});
 // write your code here
+
+class Student {
+  constructor() {
+
+  }
+
+  help() {
+    console.log('Type <insertData> to insert a student\'s data');
+    console.log('Type <updateData> to update a student\'s data');
+    console.log('Type <deleteData> to delete a student\'s data');
+    console.log('Type <listData> to list all student\'s data');
+    console.log('Type <listName> to list a student\'s data by name');
+    console.log('Type <listBy> to list a student\'s data by whatever you like');
+    console.log('Type <listMonth> to list student(s) which having birthday this month');
+    console.log('Type <listSorted> to sort students data by birthdate');
+  }
+
+  insertData(first_name, last_name, gender, birthday, email, phone) {
+    db.serialize(function() {
+      let query = `INSERT INTO student (first_name, last_name, gender, birthday, email, phone) VALUES ('${first_name}', '${last_name}', '${gender}', '${birthday}', '${email}', '${phone}')`;
+      db.run(query, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`${first_name} has been listed`);
+        }
+      });
+    });
+  }
+
+  updateData(id, first_name, last_name, gender, birthday, email, phone) {
+    db.serialize(function() {
+      let query = `UPDATE student SET first_name = "${first_name}", last_name = "${last_name}", gender = "${gender}", birthday = "${birthday}", email = "${email}", phone = "${phone}" WHERE id = "${id}"`
+      db.run(query, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Student\'s data updated");
+        }
+      });
+    });
+  }
+
+  deleteData(id) {
+    db.serialize(function() {
+      let query = `DELETE FROM student WHERE id = ${id}`
+      db.run(query, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Student has been deleted');
+        }
+      });
+    });
+  }
+
+  listData() {
+    db.serialize(function() {
+      let query = "SELECT * FROM student"
+      db.all(query, (err,rows) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(rows);
+        }
+      })
+    })
+  }
+
+  listName(name) {
+    db.serialize(function() {
+      let query = `SELECT * FROM student WHERE first_name like '%${name}%' OR last_name like '%${name}%'`
+      db.all(query, (err,rows) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(rows);
+        }
+      })
+    })
+  }
+
+  listBy(option, value) {
+    db.serialize(function() {
+      let query = `SELECT * FROM student WHERE UPPER(${option}) = UPPER("${value}")`
+      db.all(query, (err,rows) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(rows);
+        }
+      })
+    })
+  }
+
+  listMonth() {
+    db.serialize(function() {
+      let query = `SELECT * FROM student WHERE strftime('%m', birthday) = strftime('%m', date('now'))`
+      db.all(query, (err,rows) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(rows);
+        }
+      })
+    })
+  }
+
+  listSorted() {
+    db.serialize(function() {
+      let query = `SELECT * FROM student ORDER BY strftime('%m%d', birthday) ASC`
+      db.all(query, (err,rows) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(rows);
+        }
+      })
+    })
+  }
+}
+
+let studentData = new Student()
+replServer.context.help = studentData.help;
+replServer.context.insertData = studentData.insertData;
+replServer.context.updateData = studentData.updateData;
+replServer.context.deleteData = studentData.deleteData;
+replServer.context.listData = studentData.listData;
+replServer.context.listName = studentData.listName;
+replServer.context.listBy = studentData.listBy;
+replServer.context.listMonth = studentData.listMonth;
+replServer.context.listSorted = studentData.listSorted;

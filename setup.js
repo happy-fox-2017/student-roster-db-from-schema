30a88e2fa6a5b186b93259f8dd31caa427d7d6c6
myyusuf <myyusuf1911@gmail.com>
2017-06-06 08:33:16 +0700

    Release 0

--- setup.js
+++ setup.js
@@ -1,3 +1,50 @@
-"use strict"
+'use strict'
 
-//write your code here
+const repl = require('repl');
+const sqlite3 = require('sqlite3').verbose();
+
+const file = 'student.db';
+const db = new sqlite3.Database(file);
+
+const CREATE_TABLE = `
+  CREATE TABLE IF NOT EXISTS student
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      firstname TEXT NOT NULL,
+      lastname TEXT,
+      birthdate DATE
+    )`;
+
+const SEED_DATA = `
+  INSERT INTO student (firstname, lastname, birthdate) VALUES
+    ('Rubi', 'Henjaya', '1986-11-20'),
+    ('Riza', 'Fahmi', '1983-12-3')
+`;
+
+const createTable = () => {
+  db.serialize(() => {
+    db.run(CREATE_TABLE, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Table created.');
+      }
+    });
+  });
+};
+
+const seedData = () => {
+  db.serialize(() => {
+    db.run(SEED_DATA, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Data created.');
+      }
+    });
+  });
+};
+
+const replStart = repl.start('> ');
+replStart.context.createTable = createTable;
+replStart.context.seedData = seedData;
